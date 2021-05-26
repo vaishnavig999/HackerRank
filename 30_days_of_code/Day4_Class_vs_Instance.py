f0a57efe4349d8e0631ca92b3f4d44507a44f15f
Vaishnavi Gaikwad <vaishnaviig08@gmail.com>
2021-05-26 22:56:35 +0530

    30 days of code folder

---
+++ 30_days_of_code/Day4_Class_vs_Instance.py
@@ -0,0 +1,40 @@
+# Write a Person class with an instance variable, age, and a constructor that takes an integer, initialAge, as a parameter. The constructor must assign initialAge  to age after confirming the argument passed as initialAge is not negative; 
+# if a negative argument is passed as initialAge , the constructor should set age to 0 and print Age is not valid, setting age to 0.. In addition, you must write the following instance methods:
+
+# yearPasses()- should increase the age instance variable by 1.
+# amIOld()- should perform the following conditional actions:
+
+# If age<13, print You are young.
+# If age>=13 and age<18, print You are a teenager.
+# Otherwise, print You are old..
+
+class Person:
+    def __init__(self,initialAge):
+        # Add some more code to run some checks on initialAge
+        if initialAge>0:
+            self.age=initialAge
+        else:
+            print("Age is not valid, setting age to 0.")
+            self.age=0
+              
+    def amIOld(self):
+        # Do some computations in here and print out the correct statement to the console
+        if self.age<13:
+            print("You are young.")
+        elif self.age>=13 and self.age<18:
+            print("You are a teenager.")
+        else:
+            print("You are old.")
+    def yearPasses(self):
+        # Increment the age of the person in here
+        self.age+=1
+
+t = int(input())
+for i in range(0, t):
+    age = int(input())         
+    p = Person(age)  
+    p.amIOld()
+    for j in range(0, 3):
+        p.yearPasses()       
+    p.amIOld()
+    print("")
