cfe2b10714a2e0b3032430fe2d1722003109154c
Vaishnavi Gaikwad <vaishnaviig08@gmail.com>
2021-05-26 21:25:46 +0530

    upload files day1 to day3

---
+++ Day0_Hello_world.py
@@ -0,0 +1,11 @@
+
+# On the first line, we print the string literal Hello, World.. 
+# On the second line, we print the contents of the variable which, for this sample case, happens to be Welcome to 30 Days of Code!. 
+
+
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+print(input_string)
